b24b70b281bc8b0b46927eb425b52ad431f547ba
Desmond Odilinye <izudesmond48@gmail.com>
2020-11-16 21:22:19 +0100

    adjusted desmalc file

---
+++ js1/scripts.js
@@ -0,0 +1,30 @@
+    $('#Reservemodal').on('show.bs.modal', function (event) {
+  var button = $(event.reservebutton) // Button that triggered the modal
+        var recipient = button.data('reservebutton');});
+    $(document).ready(function (){
+            $('#reservebutton').click(function (){
+                $('#Reservemodal').modal('toggle');});
+        });
+    $(document).ready(function (){
+            $('#modalbutton').click(function (){
+                $('#loginModal').modal('toggle');});
+        });
+ $(document).ready(function (){
+    $('#mycarousel').carousel({interval: 2000});
+        
+    $('#carouselButton').click(function(){
+       if($('#carouselButton').children('span').hasClass('fa-pause')){
+          $('#mycarousel').carousel('pause');
+          $('#carouselButton').children('span').removeClass('fa-pause');
+        $('#carouselButton').children('span').addClass('fa-play');
+       } 
+        else  if($('#carouselButton').children('span').hasClass('fa-play')){
+          $('#mycarousel').carousel('cycle');
+          $('#carouselButton').children('span').removeClass('fa-play');
+        $('#carouselButton').children('span').addClass('fa-pause');
+        }
+        
+    });
+    });
+                               
+    
